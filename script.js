95adaff2e997ceead070d03f4f5a9cf6684f043b
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-05-11 11:33:58 +0200

    Aggiunto enter

--- script.js
+++ script.js
@@ -9,5 +9,19 @@ $('.fa-microphone').click(function(){
         //Aggiungo il template con relativo messaggio in pagina
         $('.chat_page').append($('.template .messaggio').clone())
     }
-
 })
+
+$('.invia_mes input').on('keypress',function(e) {
+    if(e.which == 13) {
+        //Creo una variabile che contiene il testo dell'input
+        var testo_messaggio = $('.invia_mes input').val()
+
+        if (testo_messaggio != '') {
+            //Inserisco il testo dell'input dentro al template da visualizzare
+            $('.template .messaggio p:first-of-type').text(testo_messaggio)
+
+            //Aggiungo il template con relativo messaggio in pagina
+            $('.chat_page').append($('.template .messaggio').clone())
+        }
+    }
+});
