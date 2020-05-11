fe2b62fe967413a7f8da3fd63459c2f7d60524d2
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-05-11 13:18:24 +0200

    No messaggi vuoti o con soli spazi

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $('.invia').click(function(){
     //Creo una variabile che contiene il testo dell'input
     var testo_messaggio = $('.invia_mes input').val()
 
-    if (testo_messaggio != '') {
+    if (/\S/.test(testo_messaggio)) {
         //Inserisco il testo dell'input dentro al template da visualizzare
         $('.template .messaggio p:first-of-type').text(testo_messaggio)
 
@@ -19,7 +19,7 @@ $('.invia_mes input').on('keypress',function(e) {
         //Creo una variabile che contiene il testo dell'input
         var testo_messaggio = $('.invia_mes input').val()
 
-        if (testo_messaggio != '') {
+        if (/\S/.test(testo_messaggio)) {
             //Inserisco il testo dell'input dentro al template da visualizzare
             $('.template .messaggio p:first-of-type').text(testo_messaggio)
 
