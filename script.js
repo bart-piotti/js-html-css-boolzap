76e197b7833131b5be8526ff46dc07d4f0cab32d
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-05-11 14:18:27 +0200

    Aggiunta l'ora ai messaggi

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 //Cliccando sull'icona "invia" viene inviato il messaggio
 $('.invia').click(function(){
+    //Creo delle variabili per ore e minuti
+    d = new Date();
+    ora = d.getHours().toString();
+    minuti = d.getMinutes().toString();
+
     //Creo una variabile che contiene il testo dell'input
     var testo_messaggio = $('.invia_mes input').val()
 
     if (/\S/.test(testo_messaggio)) {
         //Inserisco il testo dell'input dentro al template da visualizzare
         $('.template .messaggio:first-child p:first-of-type').text(testo_messaggio)
+
+        //Aggiunge l'ora al messaggio
+        if (minuti.length < 2) {
+            $('.template .messaggio:first-child p:last-of-type').text(ora + ':0' + minuti)
+        }
+        else {
+            $('.template .messaggio:first-child p:last-of-type').text(ora + ':' + minuti)
+        }
 
         //Aggiungo il template con relativo messaggio in pagina
         $('.chat_page').append($('.template .messaggio:first-child').clone())
@@ -20,12 +33,25 @@ $('.invia').click(function(){
 //Cliccando enter viene inviato il messaggio
 $('.invia_mes input').on('keypress',function(e) {
     if(e.which == 13) {
+        //Creo delle variabili per ore e minuti
+        d = new Date();
+        ora = d.getHours().toString();
+        minuti = d.getMinutes().toString();
+
         //Creo una variabile che contiene il testo dell'input
         var testo_messaggio = $('.invia_mes input').val()
 
         if (/\S/.test(testo_messaggio)) {
             //Inserisco il testo dell'input dentro al template da visualizzare
             $('.template .messaggio:first-child p:first-of-type').text(testo_messaggio)
+
+            //Aggiunge l'ora al messaggio
+            if (minuti.length < 2) {
+                $('.template .messaggio:first-child p:last-of-type').text(ora + ':0' + minuti)
+            }
+            else {
+                $('.template .messaggio:first-child p:last-of-type').text(ora + ':' + minuti)
+            }
 
             //Aggiungo il template con relativo messaggio in pagina
             $('.chat_page').append($('.template .messaggio:first-child').clone())
@@ -40,7 +66,17 @@ $('.invia_mes input').on('keypress',function(e) {
 
 });
 
+//Funzione per rispondere "Ok"
 function inviaRisposta() {
+    //Aggiunge l'ora al messaggio di risposta, aggiunge lo 0 davanti ai minuti se hanno una sola cifra
+    if (minuti.length < 2) {
+        $('.template .risposta p:last-of-type').text(ora + ':0' + minuti)
+    }
+    else {
+        $('.template .risposta p:last-of-type').text(ora + ':' + minuti)
+    }
+
+    //Dopo 3 secondi da quando è stata chiamata manda una risposta
     setTimeout(function(){
         $('.chat_page').append($('.template .risposta').clone())
     }, 3000)
