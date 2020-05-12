aceb1efe4f04309e17825265a0a25bb2a545e7bf
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-05-12 13:08:32 +0200

    Milestone 2 completata

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-//Cliccando sull'icona "invia" viene inviato il messaggio
-$('.invia').click(function(){
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// ---------------- INVIA MESSAGGIO ----------------
+function inviaMess() {
     //Creo delle variabili per ore e minuti
     d = new Date();
     ora = d.getHours().toString();
@@ -8,7 +12,7 @@ $('.invia').click(function(){
     //Creo una variabile che contiene il testo dell'input
     var testo_messaggio = $('.invia_mes input').val()
 
-    if (/\S/.test(testo_messaggio)) {
+    if (testo_messaggio.trim() != '') {
         //Inserisco il testo dell'input dentro al template da visualizzare
         $('.template .messaggio:first-child p:first-of-type').text(testo_messaggio)
 
@@ -28,44 +32,21 @@ $('.invia').click(function(){
 
         inviaRisposta()
     }
-})
+}
 
-//Cliccando enter viene inviato il messaggio
+//Cliccando sull'icona "invia" viene inviato il messaggio
+$('.invia').click(inviaMess)
+
+//Cliccando ENTER viene inviato il messaggio
 $('.invia_mes input').on('keypress',function(e) {
     if(e.which == 13) {
-        //Creo delle variabili per ore e minuti
-        d = new Date();
-        ora = d.getHours().toString();
-        minuti = d.getMinutes().toString();
-
-        //Creo una variabile che contiene il testo dell'input
-        var testo_messaggio = $('.invia_mes input').val()
-
-        if (/\S/.test(testo_messaggio)) {
-            //Inserisco il testo dell'input dentro al template da visualizzare
-            $('.template .messaggio:first-child p:first-of-type').text(testo_messaggio)
-
-            //Aggiunge l'ora al messaggio
-            if (minuti.length < 2) {
-                $('.template .messaggio:first-child p:last-of-type').text(ora + ':0' + minuti)
-            }
-            else {
-                $('.template .messaggio:first-child p:last-of-type').text(ora + ':' + minuti)
-            }
-
-            //Aggiungo il template con relativo messaggio in pagina
-            $('.chat_page').append($('.template .messaggio:first-child').clone())
-
-            //Cancello il contenuto dell'input
-            $('.invia_mes input').val('')
-
-            inviaRisposta()
-        }
+        inviaMess()
     }
-
-
 });
+// ---------------- /INVIA MESSAGGIO ----------------
 
+
+// ---------------- RISPONDI AD OGNI MESSAGGIO ----------------
 //Funzione per rispondere "Ok"
 function inviaRisposta() {
     //Aggiunge l'ora al messaggio di risposta, aggiunge lo 0 davanti ai minuti se hanno una sola cifra
@@ -79,10 +60,13 @@ function inviaRisposta() {
     //Dopo 3 secondi da quando è stata chiamata manda una risposta
     setTimeout(function(){
         $('.chat_page').append($('.template .risposta').clone())
-    }, 3000)
+    }, getRandom(1000, 5000))
 }
+// ---------------- /RISPONDI AD OGNI MESSAGGIO ----------------
 
-//Creo una variabile che cambia se l'input è on focus o meno
+
+// ---------------- SWITCH ICONA MICROFONO/TASTO INVIA ----------------
+//Creo una variabile che cambia se l'input è on focus o meno per visualizzare l'icona corretta
 var focus;
 $('.invia_mes input').focus(function(){
     $('.fa-microphone').hide()
@@ -105,3 +89,17 @@ $(document).click(function()
         $('.invia').show()
     }
 });
+// ---------------- /SWITCH ICONA MICROFONO/TASTO INVIA ----------------
+
+
+// ---------------- RICERCA CONTATTI ----------------
+$('.ricerca input').keyup(function(event){
+        $('.chat').hide()
+        $('.nome_contatto').each(function(){
+            if ($(this).text().toLowerCase().includes($('.ricerca input').val().toLowerCase())) {
+                console.log('ok');
+                $(this).closest('.chat').show()
+            }
+        })
+})
+// ---------------- /RICERCA CONTATTI ----------------
