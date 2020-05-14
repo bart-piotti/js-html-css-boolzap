6fa14c65b7387a7937431cb8e792a292adc07e64
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-05-14 11:30:54 +0200

    Milestone 3 completata

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ function inviaMess() {
     d = new Date();
     ora = d.getHours().toString();
     minuti = d.getMinutes().toString();
+    if (minuti.length < 2) {
+        minuti = '0' + minuti;
+    }
 
     //Creo una variabile che contiene il testo dell'input
     var testo_messaggio = $('.invia_mes input').val()
@@ -17,15 +20,10 @@ function inviaMess() {
         $('.template .messaggio:first-child p:first-of-type').text(testo_messaggio)
 
         //Aggiunge l'ora al messaggio
-        if (minuti.length < 2) {
-            $('.template .messaggio:first-child p:last-of-type').text(ora + ':0' + minuti)
-        }
-        else {
-            $('.template .messaggio:first-child p:last-of-type').text(ora + ':' + minuti)
-        }
+        $('.template .messaggio:first-child p:last-of-type').text(ora + ':' + minuti)
 
         //Aggiungo il template con relativo messaggio in pagina
-        $('.chat_page').append($('.template .messaggio:first-child').clone())
+        $('.chat_page.active').append($('.template .messaggio:first-child').clone())
 
         //Cancello il contenuto dell'input
         $('.invia_mes input').val('')
@@ -50,17 +48,12 @@ $('.invia_mes input').on('keypress',function(e) {
 //Funzione per rispondere "Ok"
 function inviaRisposta() {
     //Aggiunge l'ora al messaggio di risposta, aggiunge lo 0 davanti ai minuti se hanno una sola cifra
-    if (minuti.length < 2) {
-        $('.template .risposta p:last-of-type').text(ora + ':0' + minuti)
-    }
-    else {
-        $('.template .risposta p:last-of-type').text(ora + ':' + minuti)
-    }
+    $('.template .risposta p:last-of-type').text(ora + ':' + minuti)
 
     //Dopo 3 secondi da quando è stata chiamata manda una risposta
     setTimeout(function(){
-        $('.chat_page').append($('.template .risposta').clone())
-    }, getRandom(1000, 5000))
+        $('.chat_page.active').append($('.template .risposta').clone())
+    }, 1000)
 }
 // ---------------- /RISPONDI AD OGNI MESSAGGIO ----------------
 
@@ -93,13 +86,60 @@ $(document).click(function()
 
 
 // ---------------- RICERCA CONTATTI ----------------
+//Reagise ad ogni tasto premuto quando si è in input sulla barra di ricerca
 $('.ricerca input').keyup(function(event){
+        //Al primo tasto che premi nasconde le chat senza corrispondenza
         $('.chat').hide()
+        //Scorre tra tutti i nomi dei contatti ed esegue la funzione su ognuno
         $('.nome_contatto').each(function(){
-            if ($(this).text().toLowerCase().includes($('.ricerca input').val().toLowerCase())) {
-                console.log('ok');
+            //Se il testo di ".nome contatto"(this) contiene ciò che ha cercato l'utente ==>
+            if ($(this).text().toLowerCase().trim().includes($('.ricerca input').val().toLowerCase().trim())) {
+                // ==> Mostra quel contatto
                 $(this).closest('.chat').show()
             }
         })
 })
 // ---------------- /RICERCA CONTATTI ----------------
+
+
+// ---------------- OPZIONI MESSAGGIO ----------------
+$(document).on('click', '.info', function(){
+    //(this) corrisponde a .info
+    $(this).siblings('.opzioni_messaggio').toggle()
+})
+
+$('body').click(function(event){
+    if(event.target.className != 'messaggio_info') {
+        $('.opzioni_messaggio').hide()
+    }
+})
+// ---------------- /OPZIONI MESSAGGIO ----------------
+
+
+// ---------------- CANCELLA MESSAGGIO ----------------
+$(document).on('click', '.canc_mess', function(){
+    //(this) corrisponde a .canc_mess
+    $(this).closest('.messaggio').hide()
+})
+// ---------------- /CANCELLA MESSAGGIO ----------------
+
+
+// ---------------- CAMBIA CHAT CON CLICK SU CONTATTO ----------------
+$('.elenco_chat .chat').click(function(){
+    //Recuper il data del contatto su chui ho cliccato
+    data_chat = $(this).attr('data-chat')
+    console.log(data_chat);
+    //Rimuovo la classe active da tutte le chat (le nascondo tutte)
+    $('.chat_page').removeClass('active')
+    //Aggiungo la classe active alla chat con lo stesso data del contatto su cui ho cliccato
+    $('.chat_page[data-chat="' + data_chat +'"]').addClass('active')
+    //Metto nell'header della chat il nome e la foto del contatto su cui ho cliccato
+    $('.nome_foto p').text($(this).find('.nome_contatto').text())
+    $('.nome_foto img').attr('src', $(this).find('img').attr('src'))
+
+    //Rimuovo la classe active (sfondo grigio) a tutti i contatti
+    $('.chat').removeClass('active')
+    //Aggiungo la classe active al contatto su cui ho cliccato
+    $(this).addClass('active')
+})
+// ---------------- /CAMBIA CHAT CON CLICK SU CONTATTO ----------------
